1bb25204bebe935dd845ffdeddc86a120c59cc77
Raymond Guessford <raymond.guessford@thrivent.com>
2023-12-07 08:23:57 -0600

    day7p2

--- src/main/java/com/thrivent/aoc/rgaoc2023/Day7.java
+++ src/main/java/com/thrivent/aoc/rgaoc2023/Day7.java
@@ -17,16 +17,16 @@ public class Day7 {
 
     static {
         suits = Map.ofEntries(
-                entry("2", 0),
-                entry("3", 1),
-                entry("4", 2),
-                entry("5", 3),
-                entry("6", 4),
-                entry("7", 5),
-                entry("8", 6),
-                entry("9", 7),
-                entry("T", 8),
-                entry("J", 9),
+                entry("J", 0),
+                entry("2", 1),
+                entry("3", 2),
+                entry("4", 3),
+                entry("5", 4),
+                entry("6", 5),
+                entry("7", 6),
+                entry("8", 7),
+                entry("9", 8),
+                entry("T", 9),
                 entry("Q", 10),
                 entry("K", 11),
                 entry("A", 12)
@@ -37,8 +37,8 @@ public class Day7 {
         Charset charset = Charset.defaultCharset();
         List<String> stringList = Files.readAllLines(filePath, charset);
         List<Hand> handList = new ArrayList<>();
-        for (int i = 0; i < stringList.size(); i++) {
-            handList.add(parseHand(stringList.get(i)));
+        for (String s : stringList) {
+            handList.add(parseHand(s));
         }
         HandComparator comparator = new HandComparator();
         handList.sort(comparator);
@@ -51,7 +51,7 @@ public class Day7 {
         log.info("part1:{}",accumulator);
     }
     private enum HandType{
-        FIVE(7), FOUR(6), FH(5), THREE(4),TWOPAIR(3), TWO(2), ONE(1), HC(0);
+        FIVE(7), FOUR(6), FH(5), THREE(4), TWO_PAIR(3), TWO(2), ONE(1), HC(0);
         public final int power;
         HandType(int in){
             power = in;
@@ -86,28 +86,42 @@ public class Day7 {
             Integer suitIndex = suits.get(Character.toString(hand.charAt(i)));
             countHand[suitIndex]++;
         }
-        boolean hasthree = false;
-        boolean hastwo = false;
-        boolean hastwoagain = false;
-        for (int i = 0; i < countHand.length; i++) {
+        int modeIndex = -1;
+        int maxCount=0;
+        for (int i = 1; i < countHand.length; i++) {
+            if(countHand[i] > maxCount){
+                maxCount=countHand[i];
+                modeIndex=i;
+            }
+        }
+        if(modeIndex == -1) {
+            //all we found was jokers
+            countHand[12] = 5;
+        } else {
+            countHand[modeIndex] += countHand[0];
+        }
+        boolean hasThree = false;
+        boolean hasTwo = false;
+        boolean hasTwoAgain = false;
+        for (int i = 1; i < countHand.length; i++) {
             if(countHand[i] == 5){
                 return new Hand(input.split(" ")[0], Integer.parseInt(input.split(" ")[1]), HandType.FIVE );
             } else if (countHand[i] == 4) {
                 return new Hand(input.split(" ")[0], Integer.parseInt(input.split(" ")[1]), HandType.FOUR );
             } else if (countHand[i] == 3) {
-                hasthree = true;
+                hasThree = true;
             } else if (countHand[i] == 2) {
-                if(hastwo) hastwoagain = true;
-                hastwo = true;
+                if(hasTwo) hasTwoAgain = true;
+                hasTwo = true;
             }
         }
-        if (hasthree && hastwo){
+        if (hasThree && hasTwo){
             return new Hand(input.split(" ")[0], Integer.parseInt(input.split(" ")[1]), HandType.FH );
-        } else if (hasthree) {
+        } else if (hasThree) {
             return new Hand(input.split(" ")[0], Integer.parseInt(input.split(" ")[1]), HandType.THREE );
-        } else if (hastwoagain){
-            return new Hand(input.split(" ")[0], Integer.parseInt(input.split(" ")[1]), HandType.TWOPAIR );
-        }else if (hastwo){
+        } else if (hasTwoAgain){
+            return new Hand(input.split(" ")[0], Integer.parseInt(input.split(" ")[1]), HandType.TWO_PAIR);
+        }else if (hasTwo){
             return new Hand(input.split(" ")[0], Integer.parseInt(input.split(" ")[1]), HandType.TWO );
         }
         return new Hand(input.split(" ")[0], Integer.parseInt(input.split(" ")[1]), HandType.HC );
